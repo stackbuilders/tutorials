b7383f7ddfd84ebe8f2698eb820f55c947e182b4
Juan Pedro Villa Isaza <jpvillaisaza@gmail.com>
2016-05-08 17:00:15 -0500

    Add index.html and tutorials.css

--- app/Main.hs
+++ app/Main.hs
@@ -47,14 +47,48 @@ configuration =
 
 rules :: Rules ()
 rules = do
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      tutorials <- recentFirst =<< loadAll "tutorials/haskell/*/*.md"
+      let
+        archiveContext =
+          listField "tutorials" tutorialContext (return tutorials)
+            `mappend` constField "title" "Archives"
+            `mappend` defaultContext
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveContext
+        >>= loadAndApplyTemplate "templates/default.html" archiveContext
+        >>= relativizeUrls
+
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      tutorials <- recentFirst =<< loadAll "tutorials/haskell/*/*.md"
+      let
+        indexContext =
+          listField "tutorials" tutorialContext (return tutorials)
+            `mappend` constField "title" "Home"
+            `mappend` defaultContext
+
+      getResourceBody
+        >>= applyAsTemplate indexContext
+        >>= loadAndApplyTemplate "templates/default.html" indexContext
+        >>= relativizeUrls
+
+  match "stylesheets/*" $ do
+    route idRoute
+    compile compressCssCompiler
+
   match "templates/*" (compile templateCompiler)
 
   match "tutorials/haskell/*/*.md" $ do
-    route (setExtension "html")
+    route (customRoute (\i -> takeDirectory (toFilePath i) `mappend` ".html"))
     compile $
       pandocCompiler
-        >>= loadAndApplyTemplate "templates/tutorial.html" defaultContext
-        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= loadAndApplyTemplate "templates/tutorial.html" tutorialContext
+        >>= loadAndApplyTemplate "templates/default.html" tutorialContext
         >>= relativizeUrls
 
 
@@ -71,3 +105,14 @@ feedConfiguration =
     , feedRoot = ""
     , feedTitle = ""
     }
+
+
+-- |
+--
+--
+
+tutorialContext :: Context String
+tutorialContext =
+  dateField "published" "%B %e, %Y"
+    `mappend` dateField "updated" "%B %e, %Y"
+    `mappend` defaultContext
