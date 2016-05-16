376a9f923c05c321c49f446a0163825d6b4088f0
Juan Carlos Pazmiño <juankpro@outlook.com>
2016-05-16 11:33:32 -0500

    Server configuration suitable for SB website integration

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,9 @@ module Main
   )
   where
 
+-- base
 import Data.Monoid ((<>))
+import Data.List (isSuffixOf)
 
 -- filepath
 import System.FilePath
@@ -35,13 +37,28 @@ main =
 configuration :: Configuration
 configuration =
   defaultConfiguration
-    { ignoreFile = ignoreFile'
+    { ignoreFile  = ignoreFile'
+    , previewPort = 4000
     }
   where
     ignoreFile' path =
       ignoreFile defaultConfiguration path
         || takeFileName path `elem` ["makefile", "stack.yaml"]
 
+cleanIndexUrls :: Item String -> Compiler (Item String)
+cleanIndexUrls = return . fmap (withUrls cleanIndex)
+
+cleanIndexHtmls :: Item String -> Compiler (Item String)
+cleanIndexHtmls = return . fmap (replaceAll pattern replacement)
+    where
+      pattern = "/index.html"
+      replacement = const "/"
+
+cleanIndex :: String -> String
+cleanIndex url
+    | idx `isSuffixOf` url = take (length url - length idx) url
+    | otherwise            = url
+  where idx = "index.html"
 
 -- |
 --
@@ -63,6 +80,7 @@ rules = do
         >>= loadAndApplyTemplate "templates/archive.html" archiveContext
         >>= loadAndApplyTemplate "templates/default.html" archiveContext
         >>= relativizeUrls
+        >>= cleanIndexUrls
 
   match "tutorials/index.html" $ do
     route idRoute
@@ -78,20 +96,25 @@ rules = do
         >>= applyAsTemplate indexContext
         >>= loadAndApplyTemplate "templates/default.html" indexContext
         >>= relativizeUrls
+        >>= cleanIndexUrls
 
-  match "stylesheets/*" $ do
+  match "tutorials/stylesheets/*" $ do
     route idRoute
     compile compressCssCompiler
 
   match "templates/*" (compile templateCompiler)
 
   match "tutorials/haskell/*/*.md" $ do
-    route (customRoute (\i -> takeDirectory (toFilePath i) <> ".html"))
+    let
+      tutorialRoute i = takeDirectory p </> "index.html"
+        where p = toFilePath i
+    route (customRoute tutorialRoute)
     compile $
       pandocCompiler
         >>= loadAndApplyTemplate "templates/tutorial.html" tutorialContext
         >>= loadAndApplyTemplate "templates/default.html" tutorialContext
         >>= relativizeUrls
+        >>= cleanIndexUrls
 
   match "tutorials/haskell/*/*.png" $ do
     route idRoute
