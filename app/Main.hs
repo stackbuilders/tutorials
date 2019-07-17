8c796208dca0a69b2641fd917fc68cbe656c2765
Sebastián Arias <sarias@stackbuilders.com>
2019-07-17 10:11:09 -0500

    Release Python Video Generation Tutorial to Production (#95)
    * Python video generation (#87)
    
    * Tutorial for generating videos with Python
    
    Add tutorial.md file
    
    Update tutorial for sample code
    
    Update Readme instructions for pipenv shell
    
    Update in Readme.md and tutotial.md for moviepy blog
    
    Update code snippets explanation
    
    Update Tutorial to match sample code
    
    Adds code for python-video-generation tutorial
    
    Adds SB logo image
    
    Adds explanation to generate video from pipenv virtual env
    
    Add simple animation to stars for python video sample
    
    Add simple animation to stars for python video sample
    
    Add python video gif for preview
    
    Updated python video tutorial
    
    Add video.gif
    
    Update tutorial conclusions and add video sample
    
    Update template/tutorial.html so it doesnt show libraries in python blog
    
    Add python to supportedLanguages
    
    Freeze moviepy and gizeh versions on Pipfile
    
    Updates from code review
    
    Update python tutorial.md with Lori's feedback
    
    Update Tutorial with Lori's proof of reading feedback
    
    Updates Readme.md file to easy Python installation with ASDF
    
    Update python tutorial release date
    
    Center python video .gif preview in tutorial.md
    
    Update path for image video preview in python tutorial
    
    * Update path for image video preview in python tutorial (#91)
    
    * Update moviepy url link in python tutorial (#92)

--- app/Main.hs
+++ app/Main.hs
@@ -24,6 +24,7 @@ supportedLanguages :: [String]
 supportedLanguages = [  -- Name here the directories of the programming languages to be supported
     "haskell"
   , "functional-full-stack" -- Haskell backend + PureScript frontend
+  , "python"
   ]
 
 markdownPattern :: Pattern
